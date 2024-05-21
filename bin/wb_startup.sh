542ea2af5dbbe204478964e567d23a5670095123
lorenzo-campana <lorenzo.campana30@gmail.com>
2024-05-21 11:15:54 +0200

    Add data from rasp

--- bin/wb_startup.sh
+++ bin/wb_startup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-xterm -xrm '*hold: true' -geometry 132x8+0-1 -e "cd /home/widmapp/wb_controller/bin; python3 wb_controller_ultra.py" &
+xterm -xrm '*hold: true' -geometry 132x8+0-1 -e "cd /home/widmapp/waveboard/bin; python3 wb_controller_ultra.py -m -p 2-sonde.json" &
